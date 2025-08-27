3182e6a36b6009eff56efe069389d3dd2b1f7123
Djebran Lezzoum <ldjebran@gmail.com>
2025-08-27 10:27:45 +0200

    enhance lightspeed-inline-agent configuration
    Enhance lightspeed-inline-agent configuration:
    Added a min_tools configuration
    Added a system_prompt_path configuration
    Added a system_prompt configuration
    Added always_include_tools configuration
    Added a README.md file
    issues:
    - https://issues.redhat.com/browse/AAP-51325
    - https://issues.redhat.com/browse/AAP-51327
    
    Signed-off-by: Djebran Lezzoum <ldjebran@gmail.com>

--- lightspeed_stack_providers/providers/inline/agents/lightspeed_inline_agent/agents.py
+++ lightspeed_stack_providers/providers/inline/agents/lightspeed_inline_agent/agents.py
@@ -63,6 +63,5 @@ class LightspeedAgentsImpl(MetaReferenceAgentsImpl):
             ),
             created_at=agent_info.created_at,
             policy=self.policy,
-            tools_filter_model_id=self.config.tools_filter.model_id,
-            tools_filter_enabled=self.config.tools_filter.enabled,
+            tools_filter_config=self.config.tools_filter,
         )
